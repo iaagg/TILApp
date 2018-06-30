ce29bf93c9ddb22dcb2ad7d81fa76096f156f087
Aleksey Getman <getmanag@gmail.com>
2018-06-30 18:03:43 +0300

    Switched to PostgreSQL

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -1,5 +1,5 @@
 import Vapor
-import FluentSQLite
+import FluentPostgreSQL
 
 final class Acronym: Codable {
     var id: Int?
@@ -11,7 +11,7 @@ final class Acronym: Codable {
     }
 }
 
-extension Acronym: SQLiteModel {}
+extension Acronym: PostgreSQLModel {}
 
 extension Acronym: Migration {}
 
