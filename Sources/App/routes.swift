73cdcee7e663507312aacb50187c54ff1f37be58
Aleksey Getman <getmanag@gmail.com>
2018-07-01 00:42:11 +0300

    User api added

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,4 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     let acronymsController = AcronymsController()
     try router.register(collection: acronymsController)
+    
+    let usersController = UsersController()
+    try router.register(collection: usersController)
 }
