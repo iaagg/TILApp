1ddbe79c9bbb68dee841523a34d632920a9abe1d
Aleksey Getman <getmanag@gmail.com>
2018-07-01 23:09:42 +0300

    Get sibling relationships

---
+++ Sources/App/Models/AcronymCategoryPivot.swift
@@ -0,0 +1,30 @@
+import FluentPostgreSQL
+import Foundation
+
+final class AcronymCategoryPivot: PostgreSQLUUIDPivot {
+    typealias Left = Acronym
+    typealias Right = Category
+    
+    var id: UUID?
+    
+    var acronymID: Acronym.ID
+    var categoryID: Category.ID
+    
+    static let leftIDKey: LeftIDKey = \.acronymID
+    static let rightIDKey: RightIDKey = \.categoryID
+    
+    init(_ acronymID: Acronym.ID, _ categoryID: Category.ID) {
+        self.acronymID = acronymID
+        self.categoryID = categoryID
+    }
+}
+
+extension AcronymCategoryPivot: Migration {
+    static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
+        return Database.create(self, on: connection) { builder in
+            try addProperties(to: builder)
+            builder.reference(from: \.acronymID, to: \Acronym.id)
+            builder.reference(from: \.categoryID, to: \Category.id)
+        }
+    }
+}
